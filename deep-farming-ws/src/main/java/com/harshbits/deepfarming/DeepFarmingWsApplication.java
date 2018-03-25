1e8f2b90c4037dbb147083dd533b1c212c17674b
Harsh Bhavsar <hbhavsar2110@yahoo.com>
2018-03-25 13:03:26 -0500

    [1.0.0] Deep Farming Web Service Creation

---
+++ deep-farming-ws/src/main/java/com/harshbits/deepfarming/DeepFarmingWsApplication.java
@@ -0,0 +1,12 @@
+package com.harshbits.deepfarming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepFarmingWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepFarmingWsApplication.class, args);
+	}
+}
